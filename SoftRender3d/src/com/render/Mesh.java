bce2c1f7e16158938158a5b3098267903f2c2cf4
canfeng.chen@microfun.com <canfeng.chen@microfun.com>
2017-04-19 15:19:06 +0800

    11.cliping

---
+++ SoftRender3d/src/com/render/Mesh.java
@@ -0,0 +1,40 @@
+package com.render;
+import java.util.List;
+
+import com.engine.math.Matrix4f;
+
+import java.util.ArrayList;
+import java.io.IOException;
+
+public class Mesh
+{
+	private List<Vertex>  m_vertices;
+	private List<Integer> m_indices;
+	
+	public Mesh(String fileName) throws IOException
+	{
+		IndexedModel model = new OBJModel(fileName).ToIndexedModel();
+
+		m_vertices = new ArrayList<Vertex>();
+		for(int i = 0; i < model.GetPositions().size(); i++)
+		{
+			m_vertices.add(new Vertex(
+						model.GetPositions().get(i),
+						model.GetTexCoords().get(i)));
+		}
+
+		m_indices = model.GetIndices();
+	}
+
+	public void Draw(RenderContext context, Matrix4f transform, Bitmap texture)
+	{
+		for(int i = 0; i < m_indices.size(); i += 3)
+		{
+			context.drawTriangle(
+					m_vertices.get(m_indices.get(i)).Transform(transform),
+					m_vertices.get(m_indices.get(i + 1)).Transform(transform),
+					m_vertices.get(m_indices.get(i + 2)).Transform(transform),
+					texture);
+		}
+	}
+}
